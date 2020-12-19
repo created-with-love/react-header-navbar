2f4b594d70aec6e9195815eda3741dbd8dc6229a
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2020-12-20 00:21:43 +0200

    добавляю фундамент для проекта

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import './App.css';
-
+import React from 'react'
+import Navbar from './Navbar'
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <h2>navbar project setup</h2>
+    </>
+  )
 }
 
-export default App;
+export default App
